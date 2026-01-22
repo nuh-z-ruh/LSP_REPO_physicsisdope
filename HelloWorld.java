e8d6aa29a50b2090ba89420e135ba6d068e76492
nuh-z-ruh <121984595+nuh-z-ruh@users.noreply.github.com>
2026-01-21 20:50:44 -0500

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,4 @@
+package org.howard.edu.lsp.assignment1;
 /*
     Nazira Paul
 */
@@ -6,5 +7,5 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World!!");
-    }//end main
-}//end class
+    }
+}
