26b144b435b5e5c103bad2ae0cfc0924a7a29e23
nuh-z-ruh <121984595+nuh-z-ruh@users.noreply.github.com>
2026-02-06 18:09:29 -0500

    edits

---
+++ LSP_Spring26/LSP Projects/src/org/howard/edu/lsp/assignment2/ETLPipeline.java
@@ -0,0 +1,15 @@
+package org.howard.edu.lsp.assignment2;
+
+import java.io;
+
+public class ETLPipeline(){
+    public static void main(String args[]){
+            BufferedReader reader = new BufferedReader(new FileReader("products.csv"));
+            String line = "";
+
+            while((line = reader.readLine()) != null){
+
+            }//end while lines to read
+    }//end main
+}//end class
+    
