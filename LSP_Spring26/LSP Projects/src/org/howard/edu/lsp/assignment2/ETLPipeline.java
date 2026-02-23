95d46ba931a88c4d82f431b47ea3253c7cd0ef4d
nuh-z-ruh <121984595+nuh-z-ruh@users.noreply.github.com>
2026-02-23 14:21:21 -0500

    functionalCode

--- LSP_Spring26/LSP Projects/src/org/howard/edu/lsp/assignment2/ETLPipeline.java
+++ LSP_Spring26/LSP Projects/src/org/howard/edu/lsp/assignment2/ETLPipeline.java
@@ -1,13 +1,14 @@
 package org.howard.edu.lsp.assignment2;
 
-import java.io;
+import java.io.*;
 
 public class ETLPipeline(){
     public static void main(String args[]){
             BufferedReader reader = new BufferedReader(new FileReader("products.csv"));
-            String line = reader.readLine();
+            String line = reader.readLine();//line = header
+            System.out.println(line);
 
-            while((line = reader.readLine()) != null){
+            while(line != null){
                 line = reader.readLine();
                 line.toUpperCase();
 
